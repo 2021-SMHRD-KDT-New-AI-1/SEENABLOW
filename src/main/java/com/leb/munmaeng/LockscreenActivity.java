d54b69f00b2554c570c418251a92c359385e8a24
GoSungWook <ksy4717@gmail.com>
2021-10-13 13:53:14 +0900

    정보 페이지, 퀴즈 알고리즘 완성

--- src/main/java/com/leb/munmaeng/LockscreenActivity.java
+++ src/main/java/com/leb/munmaeng/LockscreenActivity.java
@@ -3,6 +3,7 @@ package com.leb.munmaeng;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class LockscreenActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class LockscreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lockscreen);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
     }
 }
