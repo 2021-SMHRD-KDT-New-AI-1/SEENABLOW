277cec46cdc2941e68f981041ca7bde9cf0d9097
GoSungWook <ksy4717@gmail.com>
2021-10-14 18:49:38 +0900

    잠금화면 완성

--- src/main/java/com/leb/munmaeng/LockscreenActivity.java
+++ src/main/java/com/leb/munmaeng/LockscreenActivity.java
@@ -2,13 +2,23 @@ package com.leb.munmaeng;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
 import java.util.Random;
@@ -18,7 +28,8 @@ public class LockscreenActivity extends AppCompatActivity {
     TextView lock_word, lock_mean;
     Button btn_start;
     List<DataVO> data;
-
+    int random, random1;
+    String word, mean;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +42,53 @@ public class LockscreenActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
                 WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
 
-        lock_word.setText("단속");
-        lock_mean.setText("규제하거나 바로잡아 이끄는 일.");
+
+        String[] arr = new String[]{"novel", "poem", "news", "column"};
+        random = new Random().nextInt(arr.length-1);
+        String url = "http://172.30.1.52:3002/";
+        url += arr[random];
+
+        if(requestQueue ==null){
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray list = new JSONArray(response);
+                            random1 = new Random().nextInt(list.length()-1);
+
+                            for (int i = 0; i < list.length(); i++) {
+
+                            }
+                            JSONObject data1 = (JSONObject) list.get(random1);
+
+                            word = data1.getString("word");
+                            mean = data1.getString("mean");
+                            lock_word.setText(word);
+                            lock_mean.setText(mean);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+
+
+ requestQueue.add(request);
+
 
         btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
